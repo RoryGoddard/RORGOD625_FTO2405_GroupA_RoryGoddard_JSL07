0918df4e17bf431e7e7d19409c4a24ba2978303b
Rory Goddard <goddardrory@gmail.com>
2024-08-01 17:20:04 +0200

    Feat: Refactor function to create elements and append them instead of using innerHTML

--- index.js
+++ index.js
@@ -18,11 +18,45 @@ document.addEventListener('DOMContentLoaded', function () {
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
       const courseName = courseNameInput ? courseNameInput.value : "a course"; // Fallback to "a course" if no input
-  
+
       if (studentName.trim() === '' || personalMessage.trim() === '') {
         alert('Please fill in all fields');
         return;
       }
+
+      // Create HTML elements for certicate 
+      const certificateHeader1El = document.createElement("h1");
+      const certificateParagraph1El = document.createElement("p");
+      const certificateHeader2El = document.createElement("h2");
+      const certificateParagraph2El = document.createElement("p");
+      const certificateHeader3El = document.createElement("h3");
+      const certificateParagraph3El = document.createElement("p");
+      const certificateImgEl = document.createElement("img");
+      const certificateParagraph4El = document.createElement("p");
+
+      // Add text and attributes to certificate elements
+      certificateHeader1El.textContent = "Certificate of Achievement";
+      certificateParagraph1El.textContent = "This is to certify that";
+      certificateHeader2El.textContent = `${studentName}`;
+      certificateParagraph2El.textContent = "Has almost completed the";
+      certificateHeader3El.textContent = `${courseName}`;
+      certificateParagraph3El.textContent = "With legendary perseverance and world-class bad-assery for never giving up 🏆";
+      certificateImgEl.setAttribute("src", "logo.png");
+      certificateImgEl.setAttribute("height", 150);
+      certificateImgEl.setAttribute("alt", "CodeSpace Logo");
+      certificateParagraph4El.textContent = `${personalMessage}`;
+
+      // 
+      certificateContent.append(
+        certificateHeader1El, 
+        certificateParagraph1El, 
+        certificateHeader2El, 
+        certificateParagraph2El, 
+        certificateHeader3El, 
+        certificateParagraph3El, 
+        certificateImgEl, 
+        certificateParagraph4El
+      );
       
       // 🚨 Generate certificate content dynamically
       certificateContent.innerHTML = `
